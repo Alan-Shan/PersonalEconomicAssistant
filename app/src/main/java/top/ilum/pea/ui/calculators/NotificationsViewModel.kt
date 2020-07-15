c6ccdb9d237e42db1f85d60c1ef21303fb392d46
Alan Shan <admin@bhod.ga>
2020-07-15 13:20:40 +0300

    Initial commit

---
+++ app/src/main/java/top/ilum/pea/ui/calculators/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package top.ilum.pea.ui.calculators
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/top/ilum/pea/ui/exchange/ExchangeFragment.kt
@@ -0,0 +1,17 @@
+package top.ilum.pea.ui.exchange
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import top.ilum.pea.R
+import top.ilum.pea.ui.home.HomeViewModel
+
+class ExchangeFragment : Fragment() {
+
+
+}
