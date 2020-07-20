c89bddab7b477a4b0747506f0459d5e145aac064
Alan Shan <admin@bhod.ga>
2020-07-20 20:14:07 +0300

    fixed exchange

---
+++ app/src/main/java/top/ilum/pea/ui/exchange/ExchangeAdapter.kt
@@ -0,0 +1,65 @@
+package top.ilum.pea.ui.exchange
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.RelativeLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item__exchange_elem.view.*
+import kotlinx.android.synthetic.main.item__exchange_elem_active.view.*
+import top.ilum.pea.R
+import top.ilum.pea.data.ExchangeElement
+
+class ExchangeAdapter(
+    elemList0: List<ExchangeElement>,
+    activeNum0: Int
+) : RecyclerView.Adapter<ExchangeAdapter.ViewHolder>() {
+
+    private var activeNum = activeNum0
+    private val elemList = elemList0
+    private val viewHoldersList = mutableListOf<ViewHolder>()
+    var isActive = false
+
+    private val layoutsList = listOf(
+        R.layout.item__exchange_elem,
+        R.layout.item__exchange_elem_active
+    )
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layoutNum = if (viewType == 1) { 1 } else { 0 }
+        isActive = viewType == 1
+        Log.e("active", activeNum.toString())
+        val root = LayoutInflater.from(parent.context)
+            .inflate(layoutsList[layoutNum], parent, false)
+        val viewHolder = ViewHolder(root)
+        Log.e("type", viewType.toString())
+        viewHolder.relativeLayout.setOnClickListener {
+            isActive = !isActive
+        }
+        viewHoldersList.add(viewHolder)
+        return viewHolder
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.elementName.text = elemList[position].name
+        holder.elementSign.text = elemList[position].sign
+    }
+
+    override fun getItemCount(): Int =
+        elemList.size
+
+    override fun getItemViewType(position: Int): Int =
+        if (position == activeNum) {
+            1
+        } else {
+            0
+        }
+
+    inner class ViewHolder(private val root: View) : RecyclerView.ViewHolder(root) {
+        val relativeLayout = root as RelativeLayout
+        var elementName: TextView = if (isActive) { root.exchange_elem__nameA } else { root.exchange_elem__name }
+        var elementSign = if (isActive) { root.exchange_elem__signA } else { root.exchange_elem__sign }
+    }
+}
--- app/src/main/java/top/ilum/pea/ui/exchange/ExchangeFragment.kt
+++ app/src/main/java/top/ilum/pea/ui/exchange/ExchangeFragment.kt
@@ -4,12 +4,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_exchange.*
+import top.ilum.pea.MainActivity
 import top.ilum.pea.R
-
+import top.ilum.pea.data.ExchangeElement
 class ExchangeFragment : Fragment() {
 
     private val siteName = "https://www.cbr-xml-daily.ru/daily_utf8.xml"
+
+    private var leftActive = 0
+    private var rightActive = 3
+
+    private val testExchangeList = listOf(
+        ExchangeElement("Российский Рубль", 1.0, "₽"),
+        ExchangeElement("Евро", 1.0, "€"),
+        ExchangeElement("Американский Доллар", 1.0, "\$"),
+        ExchangeElement("Японская Иена", 1.0, "¥"),
+        ExchangeElement("Британский фунт", 1.0, "£"),
+        ExchangeElement("Швейцарский франк", 1.0, "₣")
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,5 +36,23 @@ class ExchangeFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val leftAdapter = ExchangeAdapter(testExchangeList, leftActive)
+        val rightAdapter = ExchangeAdapter(testExchangeList, rightActive)
+
+        exchanges__recyclerLeft.layoutManager = LinearLayoutManager(MainActivity())
+        exchanges__recyclerLeft.adapter = leftAdapter
+
+        exchanges__recyclerRight.layoutManager = LinearLayoutManager(MainActivity())
+        exchanges__recyclerRight.adapter = rightAdapter
+
+        for (i in 0 until leftAdapter.itemCount) {
+            val item = leftAdapter.getItemId(i)
+            leftAdapter.notifyItemChanged(i, { Toast.makeText(MainActivity(), "number = $i", Toast.LENGTH_SHORT).show() })
+        }
     }
 }
