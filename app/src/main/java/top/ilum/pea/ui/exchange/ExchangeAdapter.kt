2ef1229ec32acbb500b5d28827cafb2f3a8a39a7
Shilonosov Alexandr <schilonosov.alex@yandex.ru>
2020-07-21 13:50:14 +0300

    Some visual fixes 1.1

--- app/src/main/java/top/ilum/pea/ui/exchange/ExchangeAdapter.kt
+++ app/src/main/java/top/ilum/pea/ui/exchange/ExchangeAdapter.kt
@@ -5,21 +5,19 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.RelativeLayout
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item__exchange_elem.view.*
-import kotlinx.android.synthetic.main.item__exchange_elem_active.view.*
 import top.ilum.pea.R
 import top.ilum.pea.data.ExchangeElement
 
 class ExchangeAdapter(
-    elemList0: List<ExchangeElement>,
-    activeNum0: Int
-) : RecyclerView.Adapter<ExchangeAdapter.ViewHolder>() {
+    private val elemList: List<ExchangeElement>,
+    private var activeNum: Int
+): RecyclerView.Adapter<ExchangeAdapter.ViewHolder>() {
 
-    private var activeNum = activeNum0
-    private val elemList = elemList0
     private val viewHoldersList = mutableListOf<ViewHolder>()
-    var isActive = false
+    private var isActive = false
 
     private val layoutsList = listOf(
         R.layout.item__exchange_elem,
@@ -30,10 +28,18 @@ class ExchangeAdapter(
         val layoutNum = if (viewType == 1) { 1 } else { 0 }
         isActive = viewType == 1
         val root = LayoutInflater.from(parent.context)
-            .inflate(layoutsList[layoutNum], parent, false)
+            .inflate(layoutsList[0], parent, false)
         val viewHolder = ViewHolder(root)
+        if (viewHolder.isHolderActive) {
+            viewHolder.relativeLayout.setBackgroundResource(R.drawable.exchange__active_item)
+        }
         viewHolder.relativeLayout.setOnClickListener {
-            isActive = !isActive
+            if (viewHolder.isHolderActive) {
+                it.setBackgroundResource(R.drawable.exchange__active_item)
+            } else {
+                it.setBackgroundResource(R.drawable.exchange__item)
+            }
+            viewHolder.isHolderActive = !viewHolder.isHolderActive
         }
         viewHoldersList.add(viewHolder)
         return viewHolder
@@ -55,8 +61,9 @@ class ExchangeAdapter(
         }
 
     inner class ViewHolder(private val root: View) : RecyclerView.ViewHolder(root) {
-        val relativeLayout = root as RelativeLayout
-        var elementName: TextView = if (isActive) { root.exchange_elem__nameA } else { root.exchange_elem__name }
-        var elementSign: TextView = if (isActive) { root.exchange_elem__signA } else { root.exchange_elem__sign }
+        val relativeLayout = root as ConstraintLayout
+        var isHolderActive = isActive
+        var elementName: TextView = root.exchange_elem__name
+        var elementSign: TextView = root.exchange_elem__sign
     }
 }
