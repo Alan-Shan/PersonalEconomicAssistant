c764c1faf57c9e47adeb8953da3a9dc5ff08c8bb
Shilonosov Alexandr <schilonosov.alex@yandex.ru>
2020-07-23 16:54:47 +0300

    Online exchanges functionality beta 1.1 A lot of small improvements

--- app/src/main/java/top/ilum/pea/ui/exchange/ExchangeFragment.kt
+++ app/src/main/java/top/ilum/pea/ui/exchange/ExchangeFragment.kt
@@ -8,7 +8,6 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -18,13 +17,12 @@ import top.ilum.pea.MainActivity
 import top.ilum.pea.R
 import top.ilum.pea.data.ExchangeElement
 import java.text.DecimalFormat
-import java.util.*
 import kotlin.math.roundToLong
 
 class ExchangeFragment : Fragment() {
 
-    lateinit var secThread: Thread
-    lateinit var runable: Runnable
+    private lateinit var secThread: Thread
+    private lateinit var runable: Runnable
 
     private val siteName = "https://www.cbr-xml-daily.ru/daily_utf8.xml"
 
@@ -33,13 +31,25 @@ class ExchangeFragment : Fragment() {
 
     private val exchangeList = mutableListOf<ExchangeElement>()
 
-    lateinit var leftElem: ExchangeElement
-    lateinit var rightElem: ExchangeElement
+    private lateinit var leftElem: ExchangeElement
+    private lateinit var rightElem: ExchangeElement
 
-    var handler: Handler = Handler(Looper.getMainLooper())
+    private var handler: Handler = Handler(Looper.getMainLooper())
 
-    var leftAdapter = ExchangeAdapter(exchangeList, leftActive, isLeft = true, exchangeParent = this)
-    var rightAdapter = ExchangeAdapter(exchangeList, rightActive, isLeft = false, exchangeParent = this)
+//    private lateinit var leftAdapter: RecyclerView.Adapter<ExchangeAdapter.ViewHolder>
+//    private lateinit var rightAdapter: RecyclerView.Adapter<ExchangeAdapter.ViewHolder>
+
+    private var leftAdapter =
+        ExchangeAdapter(exchangeList, leftActive, isLeft = true, exchangeParent = this)
+    private var rightAdapter =
+        ExchangeAdapter(exchangeList, rightActive, isLeft = false, exchangeParent = this)
+
+    private lateinit var leftLayoutManager: LinearLayoutManager
+    private lateinit var rightLayoutManager: LinearLayoutManager
+
+    private var wasExchangesLoaded = false
+
+//    private var firstRebuild = true
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -48,8 +58,16 @@ class ExchangeFragment : Fragment() {
     ): View? =
         inflater.inflate(R.layout.fragment_exchange, container, false)
 
+    override fun onPause() {
+        super.onPause()
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+//        val intentFilter: IntentFilter = IntentFilter()
+//        intentFilter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
+//        registerReceiver(networkChangeReceiver, intentFilter);
 
         loadExchanges()
 
@@ -57,24 +75,42 @@ class ExchangeFragment : Fragment() {
     }
 
     private fun onSuccessLoad() {
+        exchanges__noConnection.text = ""
+//        if (!firstRebuild) {
+//            leftElem = exchangeList[leftActive]
+//            rightElem = exchangeList[rightActive]
+//        } else {
+//            firstRebuild = false
+//        }
         leftElem = exchangeList[leftActive]
         rightElem = exchangeList[rightActive]
         changeElems(leftActive, left = true)
         changeElems(rightActive, left = false)
-        buildAdapters()
+        rebuildAdapters()
         exchanges__input.addTextChangedListener {
             changeValues()
         }
     }
 
     private fun buildAdapters() {
+        leftLayoutManager = LinearLayoutManager(MainActivity())
+        rightLayoutManager = LinearLayoutManager(MainActivity())
+
         leftAdapter = ExchangeAdapter(exchangeList, leftActive, isLeft = true, exchangeParent = this)
         rightAdapter = ExchangeAdapter(exchangeList, rightActive, isLeft = false, exchangeParent = this)
 
-        exchanges__recyclerLeft.layoutManager = LinearLayoutManager(MainActivity())
+        exchanges__recyclerLeft.layoutManager = leftLayoutManager
         exchanges__recyclerLeft.adapter = leftAdapter
 
-        exchanges__recyclerRight.layoutManager = LinearLayoutManager(MainActivity())
+        exchanges__recyclerRight.layoutManager = rightLayoutManager
+        exchanges__recyclerRight.adapter = rightAdapter
+    }
+
+    private fun rebuildAdapters() {
+        leftAdapter = ExchangeAdapter(exchangeList, leftActive, isLeft = true, exchangeParent = this)
+        rightAdapter = ExchangeAdapter(exchangeList, rightActive, isLeft = false, exchangeParent = this)
+
+        exchanges__recyclerLeft.adapter = leftAdapter
         exchanges__recyclerRight.adapter = rightAdapter
     }
 
@@ -121,6 +157,13 @@ class ExchangeFragment : Fragment() {
     private fun loadExchanges() {
         runable = Runnable {
             fun runMe() {
+                if (!wasExchangesLoaded) {
+                    handler.post {
+                        run {
+                            exchanges__loading.visibility = View.VISIBLE
+                        }
+                    }
+                }
                 fun calcElemRate(element: org.jsoup.nodes.Element): Double {
                     var value = element.getElementsByTag("Value")[0].text()
                         .replace(",", ".").toDouble()
@@ -133,40 +176,42 @@ class ExchangeFragment : Fragment() {
                 val webDoc: org.jsoup.nodes.Document
                 try {
                     webDoc = Jsoup.connect(siteName).get()
-                } catch (e: Exception){
-                    handler.post(Runnable {
-                        run {
-                            Toast.makeText(
-                                this.context,
-                                getText(R.string.exchange__badLoad),
-                                Toast.LENGTH_SHORT).show() }
-                    })
+                    wasExchangesLoaded = true
+                } catch (e: Exception) {
+                    if (!wasExchangesLoaded) {
+                        handler.post {
+                            run {
+                                exchanges__noConnection.text = getText(R.string.exchange__badLoad)
+                            }
+                        }
+                    }
                     return
                 }
                 val allCurrency = webDoc.getElementsByTag("Valute")
                 println(allCurrency[0])
                 allCurrency.forEach {
-                    val charCode = it.getElementsByTag("CharCode")[0].text().toUpperCase(Locale.ROOT)
+                    val charCode = it.getElementsByTag("CharCode")[0].text().toUpperCase()
                     val value = calcElemRate(it)
                     val name = it.getElementsByTag("Name")[0].text()
-                    val sign = charCode
+//                    val sign = charCode
 //                    var sign = ""
 //                    if (signMap.containsKey(charCode)) {
 //                        sign = signMap[charCode].toString()
 //                    }
-                    val exchangeElement = ExchangeElement(name, value, sign)
+                    val exchangeElement = ExchangeElement(name, value, charCode)
                     exchangeList.add(exchangeElement)
                 }
-                handler.post(Runnable {
+                handler.post {
+                    run {
+                        exchanges__loading.visibility = View.GONE
+                    }
+                }
+                handler.post {
                     run {
                         Log.d("Test", "thread go brr")
                         onSuccessLoad()
-                        Toast.makeText(
-                            this.context,
-                            getText(R.string.exchange__successLoad),
-                            Toast.LENGTH_SHORT).show()
                     }
-                })
+                }
             }
             runMe()
         }
@@ -174,125 +219,25 @@ class ExchangeFragment : Fragment() {
         secThread.start()
     }
 
-    private var signMap = mutableMapOf(
-        "USD" to "$",
-        "CAD" to "$",
-        "EUR" to "€",
-        "AED" to "د.إ.‏",
-        "AFN" to "؋",
-        "ALL" to "Lek",
-        "AMD" to "դր.",
-        "ARS" to "$",
-        "AUD" to "$",
-        "AZN" to "ман.",
-        "BAM" to "KM",
-        "BDT" to "৳",
-        "BGN" to "лв.",
-        "BHD" to "د.ب.‏",
-        "BIF" to "FBu",
-        "BND" to "$",
-        "BOB" to "Bs",
-        "BRL" to "R$",
-        "BWP" to "P",
-        "BYN" to "руб.",
-        "BZD" to "$",
-        "CDF" to "FrCD",
-        "CHF" to "CHF",
-        "CLP" to "$",
-        "CNY" to "CN¥",
-        "COP" to "$",
-        "CRC" to "₡",
-        "CVE" to "CV$",
-        "CZK" to "Kč",
-        "DJF" to "Fdj",
-        "DKK" to "kr",
-        "DOP" to "RD$",
-        "DZD" to "د.ج.‏",
-        "EEK" to "kr",
-        "EGP" to "ج.م.‏",
-        "ERN" to "Nfk",
-        "ETB" to "Br",
-        "GBP" to "£",
-        "GEL" to "GEL",
-        "GHS" to "GH₵",
-        "GNF" to "FG",
-        "GTQ" to "Q",
-        "HKD" to "$",
-        "HNL" to "L",
-        "HRK" to "kn",
-        "HUF" to "Ft",
-        "IDR" to "Rp",
-        "ILS" to "₪",
-        "INR" to "টকা",
-        "IQD" to "د.ع.‏",
-        "IRR" to "﷼",
-        "ISK" to "kr",
-        "JMD" to "$",
-        "JOD" to "د.أ.‏",
-        "JPY" to "￥",
-        "KES" to "Ksh",
-        "KHR" to "៛",
-        "KMF" to "FC",
-        "KRW" to "₩",
-        "KWD" to "د.ك.‏",
-        "KZT" to "тңг.",
-        "LBP" to "ل.ل.‏",
-        "LKR" to "SL Re",
-        "LTL" to "Lt",
-        "LVL" to "Ls",
-        "LYD" to "د.ل.‏",
-        "MAD" to "د.م.‏",
-        "MDL" to "MDL",
-        "MGA" to "MGA",
-        "MKD" to "MKD",
-        "MMK" to "K",
-        "MOP" to "MOP$",
-        "MUR" to "MURs",
-        "MXN" to "$",
-        "MYR" to "RM",
-        "MZN" to "MTn",
-        "NAD" to "N$",
-        "NGN" to "₦",
-        "NIO" to "C$",
-        "NOK" to "kr",
-        "NPR" to "नेरू",
-        "NZD" to "$",
-        "OMR" to "ر.ع.‏",
-        "PAB" to "B/.",
-        "PEN" to "S/.",
-        "PHP" to "₱",
-        "PKR" to "₨",
-        "PLN" to "zł",
-        "PYG" to "₲",
-        "QAR" to "ر.ق.‏",
-        "RON" to "RON",
-        "RSD" to "дин.",
-        "RUB" to "₽.",
-        "RWF" to "FR",
-        "SAR" to "ر.س.‏",
-        "SDG" to "SDG",
-        "SEK" to "kr",
-        "SGD" to "$",
-        "SOS" to "Ssh",
-        "SYP" to "ل.س.‏",
-        "THB" to "฿",
-        "TND" to "د.ت.‏",
-        "TOP" to "T$",
-        "TRY" to "TL",
-        "TTD" to "$",
-        "TWD" to "NT$",
-        "TZS" to "TSh",
-        "UAH" to "₴",
-        "UGX" to "USh",
-        "UYU" to "$",
-        "UZS" to "UZS",
-        "VEF" to "Bs.F.",
-        "VND" to "₫",
-        "XAF" to "FCFA",
-        "XOF" to "CFA",
-        "YER" to "ر.ي.‏",
-        "ZAR" to "R",
-        "ZMK" to "ZK",
-        "ZWL" to "ZWL$"
-    )
+//    private val networkChangeReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+//        override fun onReceive(context: Context?, intent: Intent?) {
+//            loadExchanges()
+//        }
+//    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putBoolean("wasLoaded", wasExchangesLoaded)
+        outState.putInt("leftActive", leftAdapter.activeNum)
+        outState.putInt("rightActive", rightAdapter.activeNum)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        if (savedInstanceState != null) {
+            wasExchangesLoaded = savedInstanceState.getBoolean("wasLoaded")
+            leftActive = savedInstanceState.getInt("leftActive")
+            rightActive = savedInstanceState.getInt("rightActive")
+        }
+        super.onViewStateRestored(savedInstanceState)
+    }
 }
